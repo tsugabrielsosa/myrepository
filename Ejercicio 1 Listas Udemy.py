45b5fd39238a1ee465554f57f07aa81984356ff5
Gabriel MAC <tsugabrielsosa@gmail.com>
2023-04-07 13:31:26 -0400

    ejercicio udemy

---
+++ Ejercicio 1 Listas Udemy.py
@@ -0,0 +1,8 @@
+lista=[20,50,"Curso","python",3.14]
+print(lista)
+VAlor1=(int(input("Ingrese Valor 1: ")))
+valor2=(int(input("Ingrese Valor 2: ")))
+lista[0]=VAlor1
+lista[1]=valor2
+print(lista)
+
